9c49b3b19d1b9cab17c80a97329d14cba80c11ec
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-05-26 23:39:45 -0300

    small updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
+    //this is actually JSX, not html
+    //it will allow to return only one element - on this example is 'div', but could be any other
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from React</h1>
     </div>
   );
 }
