277d3837fbdc85d4b9718534e3a10dfac10be77c
Sandro Miranda <sandro.miranda@protonmail.ch>
2021-05-27 23:02:20 -0300

    created header and buttonjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import Header from './components/Header'
 
-function App() {
+const App = () => {
+
   return (
     //this is actually JSX, not html
     //it will allow to return only one element - on this example is 'div', but could be any other
-    <div className="App">
-      <h1>Hello from React</h1>
+    <div className='container'>
+      <Header />
     </div>
-  );
+  )
 }
 
 export default App;
